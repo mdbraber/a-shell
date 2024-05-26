20b6d4c5f96d5853d51690095925e8c3870f6287
Maarten den Braber <m@mdbraber.com>
2024-05-26 14:56:51 +0200

    Changed Intents to run on background in-app; fixes #729

--- a-Shell/ExecuteCommandIntentHandler.swift
+++ a-Shell/ExecuteCommandIntentHandler.swift
@@ -1,9 +1,9 @@
 //
-//  IntentHandler.swift
-//  a-Shell-Intents
+//  ExecuteCommandHandler.swift
+//  a-Shell
 //
-//  Created by Nicolas Holzschuch on 15/05/2020.
-//  Copyright © 2020 AsheKube. All rights reserved.
+//  Created by Maarten den Braber on 23/05/2024.
+//  Copyright © 2024 AsheKube. All rights reserved.
 //
 
 import Intents
@@ -19,146 +19,21 @@ import ios_system
 // "<myApp> John saying hello"
 // "Search for messages in <myApp>"
 
-class IntentHandler: INExtension, ExecuteCommandIntentHandling, GetFileIntentHandling, PutFileIntentHandling
+class ExecuteCommandIntentHandler: NSObject, ExecuteCommandIntentHandling
 {
-    func resolveErrorIfNotFound(for intent: GetFileIntent, with completion: @escaping (INBooleanResolutionResult) -> Void) {
-        if let errorIfNotFound = intent.errorIfNotFound {
-            if (errorIfNotFound == true) {
-                completion(INBooleanResolutionResult.success(with: true))
-            } else {
-                completion(INBooleanResolutionResult.success(with: false))
-            }
-        } else {
-            completion(INBooleanResolutionResult.needsValue())
-        }
+    
+    let application: UIApplication
+    
+    init(application: UIApplication) {
+        self.application = application
     }
     
-    func resolveFile(for intent: PutFileIntent, with completion: @escaping ([INFileResolutionResult]) -> Void) {
-        var result: [INFileResolutionResult] = []
-        if let fileList = intent.file {
-            for file in fileList {
-                result.append(INFileResolutionResult.success(with: file))
-            }
-        } else {
-            result.append(INFileResolutionResult.needsValue())
-        }
-        completion(result)
+    // ExecuteCommandIntent
+    func resolveRunInApp(for intent: ExecuteCommandIntent, with completion: @escaping (INBooleanResolutionResult) -> Void) {
+        completion(INBooleanResolutionResult.success(with: intent.runInApp as! Bool ))
     }
     
-    let newLine = "\u{000A}"
-
-    func resolveOverwrite(for intent: PutFileIntent, with completion: @escaping (INBooleanResolutionResult) -> Void) {
-        if let overwrite = intent.overwrite {
-            completion(INBooleanResolutionResult.success(with: overwrite as! Bool))
-        } else {
-            completion(INBooleanResolutionResult.success(with: true)) // overwrite by default
-        }
-    }
-        
-    func handle(intent: PutFileIntent, completion: @escaping (PutFileIntentResponse) -> Void) {
-        guard let groupUrl = FileManager().containerURL(forSecurityApplicationGroupIdentifier:"group.com.mdbraber.a-Shell") else {
-            completion(PutFileIntentResponse(code: .failureRequiringAppLaunch, userActivity: nil))
-            return
-        }
-        if let fileList = intent.file {
-            FileManager().changeCurrentDirectoryPath(groupUrl.path)
-            // default value is success, any error will cause a failure
-            var intentResponse = PutFileIntentResponse(code: .success, userActivity: nil)
-            for file in fileList {
-                if (FileManager().fileExists(atPath: file.filename) && (intent.overwrite != 1)) {
-                    intentResponse = PutFileIntentResponse(code: .failure, userActivity: nil)
-                    intentResponse.error = "File \(file.filename) already exists."
-                    completion(intentResponse)
-                    return
-                }
-                var localURL = URL(fileURLWithPath: groupUrl.path)
-                localURL = localURL.appendingPathComponent(file.filename)
-                if let distantURL = file.fileURL {
-                    do {
-                        // request permission with secure URL:
-                        let isSecuredURL = distantURL.startAccessingSecurityScopedResource()
-                        let isReadable = FileManager().isReadableFile(atPath: distantURL.path)
-                        guard isSecuredURL && isReadable else {
-                            intentResponse = PutFileIntentResponse(code: .failure, userActivity: nil)
-                            intentResponse.error = "Could not get permission to access file \(file.filename)."
-                            completion(intentResponse)
-                            return
-                        }
-                        if (FileManager().fileExists(atPath: localURL.path) && (intent.overwrite == 1)) {
-                            try FileManager().removeItem(at: localURL)
-                        }
-                        try FileManager().copyItem(at: distantURL, to: localURL)
-                        
-                        if (isSecuredURL) {
-                            distantURL.stopAccessingSecurityScopedResource()
-                        }
-                    }
-                    catch {
-                        intentResponse = PutFileIntentResponse(code: .failure, userActivity: nil)
-                        intentResponse.error = "Could not create file \(file.filename): \(error.localizedDescription)"
-                        completion(intentResponse)
-                    }
-                } else {
-                    intentResponse = PutFileIntentResponse(code: .failure, userActivity: nil)
-                    intentResponse.error = "Could not access file \(file.filename)."
-                    completion(intentResponse)
-                }
-            }
-            completion(intentResponse)
-        } else {
-            completion(PutFileIntentResponse(code: .failure, userActivity: nil))
-        }
-    }
-    
-    func resolveFileName(for intent: GetFileIntent, with completion: @escaping (INStringResolutionResult) -> Void) {
-        var result: INStringResolutionResult
-        if let fileName = intent.fileName {
-            if (fileName.count > 0) {
-                result = INStringResolutionResult.success(with: fileName)
-            } else {
-                result = INStringResolutionResult.needsValue()
-            }
-        } else {
-            result = INStringResolutionResult.needsValue()
-        }
-        completion(result)
-    }
-    
-    func handle(intent: GetFileIntent, completion: @escaping (GetFileIntentResponse) -> Void) {
-        guard let groupUrl = FileManager().containerURL(forSecurityApplicationGroupIdentifier:"group.com.mdbraber.a-Shell") else {
-            completion(GetFileIntentResponse(code: .failureRequiringAppLaunch, userActivity: nil))
-            return
-        }
-        if let fileNames = intent.fileName {
-            // Sometimes, we get multiple lines (or a single line that ends with "\n")
-            let errorIfNotFound = intent.errorIfNotFound ?? true;
-            if let fileName = fileNames.components(separatedBy: "\n").first {
-                FileManager().changeCurrentDirectoryPath(groupUrl.path)
-                if FileManager().fileExists(atPath: fileName) {
-                    let fileURL = URL(fileURLWithPath: fileName)
-                    let intentResponse = GetFileIntentResponse(code: .success, userActivity: nil)
-                    intentResponse.file = INFile(fileURL: fileURL, filename: fileName, typeIdentifier: nil)
-                    completion(intentResponse)
-                } else if (errorIfNotFound == true) {
-                    let intentResponse = GetFileIntentResponse(code: .failure, userActivity: nil)
-                    intentResponse.message = "File \(fileName) not found error"
-                    completion(intentResponse)
-                } else {
-                    let intentResponse = GetFileIntentResponse(code: .success, userActivity: nil)
-                    completion(intentResponse)
-                }
-                return
-            }
-        }
-        let intentResponse = GetFileIntentResponse(code: .failure, userActivity: nil)
-        intentResponse.message = "No filename provided."
-        completion(intentResponse)
-    }
-    
-    func resolveOpenWindow(for intent: ExecuteCommandIntent, with completion: @escaping (EnumResolutionResult) -> Void) {
-        completion(EnumResolutionResult.success(with: intent.openWindow))
-    }
-    
+    // ExecuteCommandIntent
     func resolveKeepGoing(for intent: ExecuteCommandIntent, with completion: @escaping (INBooleanResolutionResult) -> Void) {
         if let keepGoing = intent.keepGoing {
             completion(INBooleanResolutionResult.success(with: keepGoing as! Bool))
@@ -167,12 +42,13 @@ class IntentHandler: INExtension, ExecuteCommandIntentHandling, GetFileIntentHan
         }
     }
     
+    let commandQueue = DispatchQueue(label: "executeCommand", qos: .utility)
     let sessionIdentifier = "inExtension"
     let endOfTransmission = "\u{0004}"  // control-D, used to signal end of transmission
     var response = ""
     var outputReceived = false
 
-    // Implement handlers for each intent you wish to handle.
+    // ExecuteCommandIntent
     func resolveCommand(for intent: ExecuteCommandIntent, with completion: @escaping ([INStringResolutionResult]) -> Void) {
         var result: [INStringResolutionResult] = []
         if let commands = intent.command {
@@ -195,58 +71,38 @@ class IntentHandler: INExtension, ExecuteCommandIntentHandling, GetFileIntentHan
         completion(result)
     }
     
-
-    // Commands that do not require interaction, access to local library files, access to local configuration files, access to JS...
-    // Also imageMagick commands since I have added ImageMagick configuration files. Not python by default, but users can force it.
-    let localCommands = ["awk", "calc", "cat", "chflags", "chksum", "chmod", "compress", "cp", "curl", "date", "diff", "dig", "du", "echo", "egrep", "env", "fgrep", "find", "grep", "gunzip", "gzip", "head", "host", "ifconfig", "lex", "link", "ln", "ls", "lua", "luac", "md5", "mkdir", "mv", "nc", "nslookup", "openurl", "pbcopy", "pbpaste",  "ping", "printenv", "pwd", "readlink", "rm", "rmdir", "say", "scp", "setenv", "sftp", "sort", "stat", "sum", "tail", "tar", "tee", "touch", "tr", "true", "uname", "uncompress", "uniq", "unlink", "unsetenv", "uptime", "wc", "whoami", "whois", "xargs", "xxd", "convert", "identify", "basename", "dirname", "expr"]
-
+    // ExecuteCommandIntent
     func handle(intent: ExecuteCommandIntent, completion: @escaping (ExecuteCommandIntentResponse) -> Void) {
         if let commands = intent.command {
-            var open = intent.openWindow // .open: always open the app. .close: never open the app
+            let runInApp = intent.runInApp as? Bool // .open: always open the app. .close: never open the app
             let keepGoing = intent.keepGoing as? Bool
-            if (open != .open) && (open != .close) {
-               // Should we open the App to resolve this set of commands?
-               // Make the decision based on all commands
-                for command in commands {
-                    let inner_commands = command.components(separatedBy: "\n")
-                    for inner_command in inner_commands {
-                        let arguments = inner_command.split(separator: " ")
-                        if arguments.count == 0 { continue }
-                        if localCommands.contains(String(arguments[0])) {
-                            open = .close
-                        } else {
-                            open = .open // We need to open the app
-                            break
-                        }
-                    }
-                    if (open == .open) { break }
-                }
-            }
+            
             guard let groupUrl = FileManager().containerURL(forSecurityApplicationGroupIdentifier:"group.com.mdbraber.a-Shell") else { completion(ExecuteCommandIntentResponse(code: .failureRequiringAppLaunch, userActivity: nil))
                 return
             }
-            if (open == .close) {
-                // lightweight commands (no dependency on UI, nothing fancy, done in less than 5s)
-                // Also no dependency on local files, including config. Can however create and remove files.
-                // Can be executed without opening the app.
+                
+            if (!runInApp!) {
+                // Execute without opening the app
                 FileManager().changeCurrentDirectoryPath(groupUrl.path)
-                response = ""
+                self.response = ""
                 var result:Int32 = 0
-                for command in commands {
-                    result = executeCommandInExtension(command: command)
-                    if (result != 0) && (keepGoing != nil) && (!keepGoing!) { break } // Stop executing after an error
+                commandQueue.async {
+                    for command in commands {
+                        result = self.executeCommandInExtension(command: command)
+                        if (result != 0) && (keepGoing != nil) && (!keepGoing!) { break } // Stop executing after an error
+                    }
+                    var intentResponse: ExecuteCommandIntentResponse
+                    // If keepGoing is set, don't stop the Shortcut even if the commands have failed.
+                    if (result == 0 || ((keepGoing != nil) && keepGoing!)) {
+                        let activity = NSUserActivity(activityType: "com.mdbraber.a-Shell.ExecuteCommand")
+                        intentResponse = ExecuteCommandIntentResponse(code: .success, userActivity: activity)
+                    } else {
+                        intentResponse = ExecuteCommandIntentResponse(code: .failure, userActivity: nil)
+                    }
+                    intentResponse.property = self.response
+                    intentResponse.property1 = NSNumber(value: result)
+                    completion(intentResponse)
                 }
-                var intentResponse: ExecuteCommandIntentResponse
-                // If keepGoing is set, don't stop the Shortcut even if the commands have failed.
-                if (result == 0 || ((keepGoing != nil) && keepGoing!)) {
-                    let activity = NSUserActivity(activityType: "com.mdbraber.a-Shell.ExecuteCommand")
-                    intentResponse = ExecuteCommandIntentResponse(code: .success, userActivity: activity)
-                } else {
-                    intentResponse = ExecuteCommandIntentResponse(code: .failure, userActivity: nil)
-                }
-                intentResponse.property = response
-                intentResponse.property1 = NSNumber(value: result)
-                completion(intentResponse)
             } else {
                 // other commands --> sent to the application itself
                 var urlString = ""
@@ -265,6 +121,7 @@ class IntentHandler: INExtension, ExecuteCommandIntentHandling, GetFileIntentHan
         completion(ExecuteCommandIntentResponse(code: .failure, userActivity: nil))
     }
     
+    // ExecuteCommandIntent
     func executeCommandInExtension(command: String) -> Int32 {
         // set up streams for feedback:
         // Create new pipes for our own stdout/stderr
@@ -294,6 +151,8 @@ class IntentHandler: INExtension, ExecuteCommandIntentHandling, GetFileIntentHan
         ios_setStreams(stdin_file, stdout_file, stdout_file)
         // Execute command (remove spaces at the beginning and end):
         // reset the LC_CTYPE (some commands (luatex) can change it):
+        
+        /*
         setenv("LC_CTYPE", "UTF-8", 1);
         setlocale(LC_CTYPE, "UTF-8");
         // Environment variables for configuration files that are inside the main app dir:
@@ -321,6 +180,8 @@ class IntentHandler: INExtension, ExecuteCommandIntentHandling, GetFileIntentHan
         // We don't add the default PATH because 1) it's useless and 2) it causes PATH to add to itself infinitely.
         setenv("PATH", newPath.toCString(), 1)
         // End environment variables
+        */
+         
         outputReceived = false
         var returnVal: Int32 = 0
         let commands = command.components(separatedBy: "\n")
@@ -334,6 +195,7 @@ class IntentHandler: INExtension, ExecuteCommandIntentHandling, GetFileIntentHan
                 returnVal = ios_getCommandStatus()
             }
         }
+        
         fflush(thread_stdout)
         // Send info to the stdout handler that the command has finished:
         // let readOpen = fcntl(self.stdout_pipe!.fileHandleForReading.fileDescriptor, F_GETFD)
@@ -357,7 +219,8 @@ class IntentHandler: INExtension, ExecuteCommandIntentHandling, GetFileIntentHan
         }
         return returnVal
     }
-        
+     
+    // ExecuteCommandIntent
     private func onStdout(_ stdout: FileHandle) {
         if (outputReceived) { return } // don't try to read after EOT
         let data = stdout.availableData
@@ -398,13 +261,5 @@ class IntentHandler: INExtension, ExecuteCommandIntentHandling, GetFileIntentHan
             NSLog("Couldn't convert data in stdout: \(data)")
         }
     }
-    
-    override func handler(for intent: INIntent) -> Any {
-        // This is the default implementation.  If you want different objects to handle different intents,
-        // you can override this and return the handler you want for that particular intent.
-        return self
-    }
-        
-    // Once resolution is completed, perform validation on the intent and provide confirmation (optional).
     
 }
